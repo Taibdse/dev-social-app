84442c4ae86a3c6121d17e5236c2f8071bd0608e
taibdse <buiductai232@gmail.com>
2023-02-19 21:08:12 +0700

    add test controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -3,10 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  test(): string {
+    return 'This is a test controller';
   }
 }
