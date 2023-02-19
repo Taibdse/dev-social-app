ea22798e326c4692bf8b044afdf37d7fcb1f7be6
taibdse <buiductai232@gmail.com>
2023-02-19 15:26:00 +0700

    add deploy script to package.json

--- server/src/modules/auth/strategies/local.strategy.ts
+++ server/src/modules/auth/strategies/local.strategy.ts
@@ -4,7 +4,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
-export class LocalStratergy extends PassportStrategy(Strategy, 'local1') {
+export class LocalStratergy extends PassportStrategy(Strategy, 'local') {
   constructor(private authService: AuthService) {
     super();
   }
